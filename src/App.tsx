7f31b178b9d1f3b86925d782657352385096a774
Matthew <matthewgoh2004@gmail.com>
2024-07-12 14:27:12 +0800

    updated & refactored sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App: React.FC = () => {
       <main className="">
         {/* <Table /> */}
       </main>
-      <NewStaffModal />
+      {/* <NewStaffModal /> */}
     </div>
   );
 };
---
+++ src/components/SidebarButton.tsx
@@ -0,0 +1,31 @@
+// SidebarButton.tsx
+import React from 'react';
+
+interface SidebarButtonProps {
+  name: string;
+  icon: React.ReactNode;
+  onClick: () => void;
+  children?: React.ReactNode;
+}
+
+const SidebarButton: React.FC<SidebarButtonProps> = ({
+  name,
+  icon,
+  onClick,
+  children,
+}) => {
+  return (
+    <li className="m-2">
+      <button
+        onClick={onClick}
+        className="block flex py-2 pl-4 hover:bg-gray-200 active:bg-gray-300 transition-colors duration-200 hover:text-gray-600 rounded-lg focus:outline-none w-full text-left"
+      >
+        {icon}
+        <span className="font-semibold ml-2">{name}</span>
+      </button>
+      {children}
+    </li>
+  );
+};
+
+export default SidebarButton;
