e2e90aacc0c9a5d5d84874095a6962718241e3d7
Matthew <matthewgoh2004@gmail.com>
2024-08-01 15:06:24 +0800

    updated docker logic

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ const port = 5000;
 
 // PostgreSQL connection
 const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'localhost', // 'db' is the service name in docker-compose
-  database: process.env.DB_NAME || 'tienhock',
-  password: process.env.DB_PASSWORD || 'foodmaker',
-  port: parseInt(process.env.DB_PORT || 5432),
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 app.use(cors());
