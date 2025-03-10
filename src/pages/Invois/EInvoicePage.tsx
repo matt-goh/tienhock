77e0d6127e076ad90c994de87c5e4ff72beec6b5
Matthew <matthewgoh2004@gmail.com>
2025-03-10 23:30:59 +0800

    refactor: add EInvoiceSubmitPage component and update invoice submission flow

--- src/pages/Invois/EInvoicePage.tsx
+++ src/pages/Invois/EInvoicePage.tsx
@@ -2,13 +2,15 @@
 import React from "react";
 import Tab from "../../components/Tab";
 import EInvoiceConsolidatedPage from "./EInvoiceConsolidatedPage";
+import EInvoiceSubmitPage from "./EInvoiceSubmitPage";
 import EInvoiceHistoryPage from "./EInvoiceHistoryPage";
 
 const EInvoicePage: React.FC = () => {
   return (
     <div className="flex flex-col px-12">
-      <Tab labels={["Consolidated", "History"]}>
+      <Tab labels={["Consolidate", "Submit", "History"]}>
         <EInvoiceConsolidatedPage />
+        <EInvoiceSubmitPage />
         <EInvoiceHistoryPage />
       </Tab>
     </div>
