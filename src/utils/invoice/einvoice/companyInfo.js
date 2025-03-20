abbdf51a7685c0d5590c79221db84e5ce018babe
Matthew <matthewgoh2004@gmail.com>
2025-03-20 20:44:30 +0800

    feat: update company information to TIEN HOCK in invoice-related components and services

--- src/utils/invoice/einvoice/companyInfo.js
+++ src/utils/invoice/einvoice/companyInfo.js
@@ -1,25 +1,26 @@
 // src/utils/invoice/einvoice/companyInfo.js
 
-export const COMPANY_INFO = {
-    // Common fields
-    name: "TIEN HOCK FOOD INDUSTRIES S/B",
-    tin: "C21636482050",
-    reg_no: "201101025173",
-    msic_code: "10741",
-    msic_description: "Manufacture of meehoon, noodles and other related products",
-    phone: "0168329291",
-    email: "tienhockfood@gmail.com",
-    postcode: "88811",
-    
-    // XML-specific formatting
-    sst_id_xml: "-",
-    address_xml: "CL.215145645, KG KIBABAIG, PENAMPANG",
-    city_xml: "KOTA KINABALU",
-    country_code: "12", // Sabah code for XML template
-    
-    // PDF-specific formatting
-    sst_id_pdf: "-",
-    address_pdf: "CL.215145645, Kg. Kibabaig, Penampang, Kota Kinabalu, Sabah",
-    city_pdf: "Kota Kinabalu",
-    state_pdf: "Sabah"
-  };
+export const TIENHOCK_INFO = {
+  // Common fields
+  name: "TIEN HOCK FOOD INDUSTRIES S/B",
+  tin: "C21636482050",
+  reg_no: "201101025173",
+  msic_code: "10741",
+  msic_description:
+    "Manufacture of meehoon, noodles and other related products",
+  phone: "0168329291",
+  email: "tienhockfood@gmail.com",
+  postcode: "88811",
+
+  // XML-specific formatting
+  sst_id_xml: "-",
+  address_xml: "CL.215145645, KG KIBABAIG, PENAMPANG",
+  city_xml: "KOTA KINABALU",
+  country_code: "12", // Sabah code for XML template
+
+  // PDF-specific formatting
+  sst_id_pdf: "-",
+  address_pdf: "CL.215145645, Kg. Kibabaig, Penampang, Kota Kinabalu, Sabah",
+  city_pdf: "Kota Kinabalu",
+  state_pdf: "Sabah",
+};
