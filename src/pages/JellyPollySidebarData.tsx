3315e082db5df9230f013f2fb492aa2dd41cf46a
Matthew <matthewgoh2004@gmail.com>
2025-04-10 21:01:48 +0800

    refactor: restructure sidebar data to include invoice routes and components for better organization

--- src/pages/JellyPollySidebarData.tsx
+++ src/pages/JellyPollySidebarData.tsx
@@ -1,28 +1,33 @@
 import { IconFileInvoice } from "@tabler/icons-react";
 import { SidebarItem } from "./pagesRoute";
+import InvoiceListPage from "./JellyPolly/InvoiceListPage";
+import InvoiceFormPage from "./JellyPolly/InvoiceFormPage";
+import InvoiceDetailsPage from "./JellyPolly/InvoiceDetailsPage";
 
 export const JellyPollySidebarData: SidebarItem[] = [
   {
     name: "Sales",
     icon: IconFileInvoice,
     subItems: [
-      // {
-      //   name: "Invoice",
-      //   path: "/sales/invoice",
-      //   component: InvoicePage,
-      //   subItems: [
-      //     {
-      //       name: "Invoice Details",
-      //       path: "/sales/invoice/:id",
-      //       component: InvoiceDetailsPage,
-      //     },
-      //     {
-      //       name: "Create New Invoice Page",
-      //       path: "/sales/invoice/create",
-      //       component: InvoiceDetailsPage,
-      //     },
-      //   ],
-      // },
+      {
+        name: "Invoice",
+        path: "/sales/invoice",
+        component: InvoiceListPage,
+        subItems: [
+          // Route for creating a new invoice
+          {
+            name: "Create New Invoice",
+            path: "/sales/invoice/new",
+            component: InvoiceFormPage,
+          },
+          // Route for viewing an existing invoice
+          {
+            name: "Invoice Details",
+            path: "/sales/invoice/:id",
+            component: InvoiceDetailsPage,
+          },
+        ],
+      },
     ],
   },
 ];
